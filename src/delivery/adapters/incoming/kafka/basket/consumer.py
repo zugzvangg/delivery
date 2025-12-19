5ee9bd2994e7b3ef1892a8bb9f6a8644cd7b9e7f
Dmitriy Bazanov <zugzvangg1@gmail.com>
2025-12-20 00:22:00 +0300

    add all to one network

---
+++ src/delivery/adapters/incoming/kafka/basket/consumer.py
@@ -0,0 +1,32 @@
+from fastapi import Depends
+from faststream.kafka.fastapi import KafkaRouter
+
+from src.delivery.core.application.use_cases.commands.create_order_command import (
+    CreateOrderCommand,
+    CreateOrderUseCase,
+)
+
+# from .schemas import BasketConfirmedEvent
+
+# Инициализация брокера
+router = KafkaRouter(bootstrap_servers="kafka:9092", include_in_schema=False)
+
+
+@router.subscriber(
+    "baskets.events",
+    group_id="basket-confirmed-group",
+)
+async def process_basket_confirmed(
+    # msg: BasketConfirmedEvent,
+    msg,
+) -> None:
+    """
+    Обработчик события подтверждения корзины.
+    """
+    use_case: CreateOrderUseCase = CreateOrderUseCase()
+    await use_case.handle(
+        CreateOrderCommand(
+            order_id=msg.basket_id, street=msg.address.street, volume=msg.volume
+        )
+
+    )
